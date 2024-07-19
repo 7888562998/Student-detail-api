1d3fbfc0fa49fec1818ac8b29f20208f16744baa
jaskaran <js4368621@gmail.com>
2024-07-19 13:54:01 +0530

    samll changes

--- index.js
+++ index.js
@@ -12,13 +12,29 @@ app.use(express.json());
 app.use("/api/v1", studentRouter);
 app.use("/api/v1", registerRouter);
 
+// const start = async () => {
+//   await connectDB();
+//   app.get("/", (req, res) => {
+//     res.send("hello from server side");
+//   });
+// };
 
 const start = async () => {
-connectDB()
-app.get("/",(req,res)=>{
-  res.send("hello from server side")
-})
-}
+  try {
+    await connectDB();
+    console.log("Database connected successfully");
+
+    app.get("/", (req, res) => {
+      res.send("hello from server side");
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+};
 start();
 // app.listen(port, () => {
 //   console.log(`connection is live to this port ${port}`);
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
 
+uri =
+ "mongodb+srv://js4368621:QlsmnziWRHks69ci@cluster0.ch28xho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const connectDB =()=>{
-  mongoose
-  .connect(
-    "mongodb+srv://js4368621:QlsmnziWRHks69ci@cluster0.ch28xho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("connection successful");
-  })
-  .catch((e) => {
-    console.log("no connection", e);
-  });
-}
+const connectDB = () => {
+    console.log("connect db")
+  return mongoose.connect(uri);
+};
 
 module.exports={connectDB};
 
