30366950ea1ae2b4e6981a6f1650ae239e409ddf
jaskaran <js4368621@gmail.com>
2024-07-19 14:44:02 +0530

    samll changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const studentRouter = require("./routes/student");
 const registerRouter = require("./routes/UserRegister");
 var cors = require("cors");
 const { connectDB } = require("./src/db/conn");
+const StudentDetail = require("./src/models/student");
 
 const app = express();
 const port = process.env.Port || 3000;
@@ -12,14 +13,20 @@ app.use(express.json());
 app.use("/api/v1", studentRouter);
 app.use("/api/v1", registerRouter);
 
-
-const start =  () => {
-  connectDB();
-  app.get("/",(req,res)=>{
-    res.send("hello from server side")
-  })
-  }
-  start();
+const start = () => {
+  connectDB()
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+    });
+  app.get("/", async(req, res) => {
+    const students = await StudentDetail.find({});
+    res.status(200).json(students);
+  });
+};
+start();
 app.listen(port, () => {
   console.log(`connection is live to this port ${port}`);
 });
