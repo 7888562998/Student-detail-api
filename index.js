2c509e2ed5f526095fba1611c3c6fc7bb093a951
jaskaran <js4368621@gmail.com>
2024-07-19 13:27:04 +0530

    samll changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const studentRouter = require("./routes/student");
-const registerRouter = require("./routes/UserRegister");
+const studentRouter = require("./src/routes/student");
+const registerRouter = require("./src/routes/UserRegister");
 var cors = require("cors");
-const { connectDB } = require("./db/conn");
+const { connectDB } = require("./src/db/conn");
 
 const app = express();
 const port = process.env.Port || 3000;
@@ -12,13 +12,14 @@ app.use(express.json());
 app.use("/api/v1", studentRouter);
 app.use("/api/v1", registerRouter);
 
+
+const start = async () => {
+connectDB()
 app.get("/",(req,res)=>{
   res.send("hello from server side")
 })
-const start = async () => {
-connectDB()
 }
 start();
-// app.listen(port, () => {
-//   console.log(`connection is live to this port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`connection is live to this port ${port}`);
+});
